55ac1800fee835b3c410d784a5594d1d5ea0a46d
Akshithaaashi <akshitha.aashi@gmail.com>
2021-08-11 15:46:48 +0530

    Added : tesst case in try catch block

--- src/test/java/MoodAnalyser2/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyser2/MoodAnalyserTest.java
@@ -4,26 +4,30 @@ import org.junit.Test;
 
 
 public class MoodAnalyserTest {
-	@Test
-	public void givenMessgae_WhenSad_ShouldReturn_Sad() throws MoodAnalyserException {
-		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood"); //passed message to constructor
-		String mood = moodAnalyser.analyseMood();
-		Assert.assertEquals("SAD", mood);
+	//Test case for Sad Mood
+		@Test
+		public void givenMessgae_WhenSad_ShouldReturn_Sad() throws MoodAnalyserException {
+			MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood"); //passed message to constructor
+			String mood = moodAnalyser.analyseMood();
+			Assert.assertEquals("SAD", mood);
+		}
+		
+		//Test case for Happy Mood
+		@Test
+		public void givenMessage_whenNotSad_shouldReturn_Happy() throws MoodAnalyserException {
+			MoodAnalyser moodAnalyser = new MoodAnalyser("I am in any Mood"); //passed message to constructor
+			String mood = moodAnalyser.analyseMood();
+			Assert.assertEquals("HAPPY", mood);
+		}
+		
+		//Test case for Null
+		@Test
+		public void givenMessage_whenNull_shouldThrow_MoodAnalysisException() {
+			try {
+				MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+				moodAnalyser.analyseMood();
+			} catch (MoodAnalyserException e) {
+				Assert.assertEquals("Invalid", e.getMessage());
+			}
+		}
 	}
-	
-	//Test case for Happy Mood
-	@Test
-	public void givenMessage_whenNotSad_shouldReturn_Happy() throws MoodAnalyserException {
-		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in any Mood"); //passed message to constructor
-		String mood = moodAnalyser.analyseMood();
-		Assert.assertEquals("HAPPY", mood);
-	}
-	
-	//Test case for Null
-	@Test
-	public void givenMessage_whenNull_shouldReturn_Null() throws MoodAnalyserException {
-		MoodAnalyser moodAnalyser = new MoodAnalyser(); //passed message to constructor
-		String mood = moodAnalyser.analyseMood();
-		Assert.assertEquals("HAPPY", mood);
-	}
-}
