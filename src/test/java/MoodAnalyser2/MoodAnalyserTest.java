e2f47e156256a11d5afa871defe0287a9074ef15
Akshithaaashi <akshitha.aashi@gmail.com>
2021-08-11 15:25:24 +0530

    Added : null mood should return happy

--- src/test/java/MoodAnalyser2/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyser2/MoodAnalyserTest.java
@@ -7,19 +7,26 @@ public class MoodAnalyserTest {
 
 
 	//Test case for Sad Mood
-	@Test
-	public void givenMessgae_WhenSad_ShouldReturn_Sad() {
-		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood"); //passed message to constructor
-		String mood = moodAnalyser.analyseMood();
-		Assert.assertEquals("SAD", mood);
+		@Test
+		public void givenMessgae_WhenSad_ShouldReturn_Sad() {
+			MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood"); //passed message to constructor
+			String mood = moodAnalyser.analyseMood();
+			Assert.assertEquals("SAD", mood);
+		}
+		
+		//Test case for Happy Mood
+		@Test
+		public void givenMessage_whenNotSad_shouldReturn_Happy() {
+			MoodAnalyser moodAnalyser = new MoodAnalyser("I am in any Mood"); //passed message to constructor
+			String mood = moodAnalyser.analyseMood();
+			Assert.assertEquals("HAPPY", mood);
+		}
+		
+		//Test case for Null
+		@Test
+		public void givenMessage_whenNull_shouldReturn_Null() {
+			MoodAnalyser moodAnalyser = new MoodAnalyser(); //passed message to constructor
+			String mood = moodAnalyser.analyseMood();
+			Assert.assertEquals("HAPPY", mood);
+		}
 	}
-	
-	//Test case for Sad Mood
-	@Test
-	public void givenMessage_whenNotSad_shouldReturn_Happy() {
-		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in any Mood"); //passed message to constructor
-		String mood = moodAnalyser.analyseMood();
-		Assert.assertEquals("HAPPY", mood);
-	}
-	
-}
